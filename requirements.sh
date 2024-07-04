8af9c12d23d8f09acbad71c7f27351f7bb2d653f
osier0524 <liubhosier@gmail.com>
2024-07-04 16:36:57 +0000

    DBA3 result

--- requirements.sh
+++ requirements.sh
@@ -6,4 +6,6 @@ pip install torch==2.2.0+cu121 torchvision==0.17.0+cu121 -f https://download.pyt
 
 pip install pyyaml numpy==1.19.5 networkx==2.6.3 Pillow==8.4.0
 
+conda install -c conda-forge openmpi
+
 conda install -c conda-forge mpi4py
