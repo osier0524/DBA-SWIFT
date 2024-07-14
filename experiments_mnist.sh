ea9ca1671e62b7d498da658d47899b7f399a2f2a
osier0524 <liubhosier@gmail.com>
2024-07-14 21:43:34 +0000

    result

--- experiments_mnist.sh
+++ experiments_mnist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for num in {1..6}
+for num in {0..6}
 do
-    mpirun --oversubscribe -np 40 python Train.py --config Config/swift-iid-DBA-mnist-ring-$num.yaml --randomSeed 100
+    mpirun --oversubscribe -np 40 python Train.py --config Config/swift-iid-DBA-mnist-clique-$num.yaml --randomSeed 100
 done
