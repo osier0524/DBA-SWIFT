f6571df43c57545c2574ff0bfd8fd0cda7af3147
osier0524 <liubhosier@gmail.com>
2024-08-13 17:21:56 +0000

    all

--- get_data_10.sh
+++ get_data_10.sh
@@ -3,5 +3,4 @@
 for seed in {0..100..10}
 do
     mpirun --oversubscribe -np 10 python Train.py --config Config/dsgd-iid-DBA-1-global-200-10nodes-dataset.yaml --randomSeed $seed
-    mpirun --oversubscribe -np 10 python Train.py --config Config/swift-iid-DBA-ring-1-global-200-10nodes-dataset.yaml --randomSeed $see
 done
