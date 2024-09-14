665645615990799304d6345d9b365b8e1eec8c97
osier0524 <liubhosier@gmail.com>
2024-09-14 19:29:44 +0000

    res

--- trigger_size_effects.sh
+++ trigger_size_effects.sh
@@ -2,5 +2,5 @@
 
 for size in {1..12}
 do
-    mpirun --oversubscribe -np 10 python Train.py --config Config/trigger_size_effects/swift-iid-DBA-mnist-ring-trigger-size$size.yaml --randomSeed 100
+    mpirun --oversubscribe -np 10 python Train.py --config Config/trigger_cifar/size_effects/swift-iid-DBA-cifar-ring-trigger-size$size.yaml --randomSeed 100
 done
